e0b0f5f60062bd19c8422aff08bb8907431edb03
Vaclav Pavlicek <vaclav@pavlicek.cz>
2013-12-03 13:24:47 +0100

    Vygeneruje náhodné číslo

---
+++ src/testcisla/TestCisla.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testcisla;
+
+/**
+ *
+ * @author Venda
+ */
+public class TestCisla {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int cislo = 0;
+        
+        cislo = 1 + (int) (100 * Math.random());    // Získá náhodné číslo od 0 do 100.
+        
+        if (cislo % 2 == 0){    //Testuje zda je proměná cislo sudá.
+        
+            System.out.println("Dostali jste sudé číslo " + cislo); // Proměná cislo je sudá
+        
+        }else{
+        
+            System.out.println("Dostali jste liché čříslo " + cislo);   // Proměná cislo je lichá
+            
+        }
+                       
+    }
+    
+}
